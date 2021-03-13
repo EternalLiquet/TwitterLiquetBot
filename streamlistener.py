758f2a8aaa02699094180f2e7c1310187fbc9cdd
Aldrin Dominique Natividad <aldmnatividad@gmail.com>
2021-03-13 13:40:12 -0500

    Save progress

--- streamlistener.py
+++ streamlistener.py
@@ -6,50 +6,68 @@ class StreamListener(tweepy.StreamListener):
         self.api = api
         self.me = api.me()
 
-    
-    
-    def on_status(self, tweet):
-        if tweet.in_reply_to_status_id is not None or \
-            tweet.user.id == self.me.id:
-            return
+    def return_divider(self):
+        return "\n=====================================\n"
+
+    def return_bot_string(self, tweet, bot_chance, found_words):
+        return f"{tweet.user.screen_name} is likely a bot! Reason: Bot chance is at {bot_chance}% with words: {found_words} in description: {tweet.user.description}\n"
+
+    def return_unfavorable_desc_string(self, tweet, unfavorability_index, found_words):
+        return f"{tweet.user.screen_name} has an unfavorable description at {unfavorability_index} and unlikely to produce good content! Words {found_words} in description: {tweet.user.description}\n"
+
+    def determine_bot_chance(self, tweet):
+        bot_chance = 0
+        found_words = []
         with open("bot_words.txt", "r", encoding="utf-8") as bot_words_file:
             file_contents = bot_words_file.read()
             bot_words = file_contents.splitlines()
-        with open("unfavorable_descriptions.txt", "r", encoding="utf-8") as red_flag_words_file:
-            file_contents = red_flag_words_file.read()
-            red_flag_words = file_contents.splitlines()
-        bot_chance = 0
-        unfavorable_description = 0
-        found_words = []
-        unfavorable_words = []
         if tweet.user.description is not None:
             for word in bot_words:
                 if word.lower() in tweet.user.description.lower():
                     bot_chance += 20
                     found_words.append(word)
+        if bot_chance >= 60:
+            print(self.return_bot_string(tweet, bot_chance, found_words))
+            print(self.return_divider())
+            with open("bots_found.txt", "a+", encoding="utf-8") as bots_found_file:
+                bots_found_file.seek(0)
+                current_file = bots_found_file.read()
+                if tweet.user.description not in current_file:
+                    bots_found_file.write(self.return_bot_string(tweet, bot_chance, found_words))
+        return bot_chance
+    
+    def determine_unfavorability(self, tweet):
+        unfavorability_index = 0
+        found_words = []
+        with open("red_flag_words.txt", "r", encoding="utf-8") as red_flag_words_file:
+            file_contents = red_flag_words_file.read()
+            red_flag_words = file_contents.splitlines()
+        if tweet.user.description is not None:
             for word in red_flag_words:
                 if word.lower() in tweet.user.description.lower():
-                    unfavorable_description += 20
-                    unfavorable_words.append(word)
-        if bot_chance >= 60:
-            bot_string = f"{tweet.user.screen_name} is likely a bot! Reason: Bot chance is at {bot_chance}% with words: {found_words} in description: {tweet.user.description}\n"
-            print(bot_string)
-            print("\n=====================================\n")
-            f = open("bots_found.txt", "a", encoding="utf-8")
-            r = open("bots_found.txt", "r", encoding="utf-8")
-            current_file = r.read()
-            if tweet.user.description not in current_file:
-                f.write(bot_string)
+                    unfavorability_index += 15
+                    found_words.append(word)
+        if unfavorability_index >= 60:
+            print(self.return_unfavorable_desc_string(tweet, unfavorability_index, found_words))
+            print(self.return_divider())
+            with open("unfavorable_accounts.txt", "a+", encoding="utf-8") as unfavorable_acc_file:
+                unfavorable_acc_file.seek(0)
+                current_file = unfavorable_acc_file.read()
+                if tweet.user.description not in current_file:
+                    unfavorable_acc_file.write(self.return_unfavorable_desc_string(tweet, unfavorability_index, found_words))
+            with open("unfavorable_descriptions_only.txt", "a+", encoding="utf-8") as descriptions_file:
+                descriptions_file.seek(0)
+                current_file = descriptions_file.read()
+                if tweet.user.description not in current_file:
+                    descriptions_file.write(f"{tweet.user.description}\n")
+        return unfavorability_index
+    
+    def on_status(self, tweet):
+        if tweet.in_reply_to_status_id is not None or tweet.user.id == self.me.id:
             return
-        if unfavorable_description >= 60:
-            unfavored_string = f"{tweet.user.screen_name} has an unfavorable description and unlikely to produce good content! Words {unfavorable_words} in description: {tweet.user.description}\n"
-            print(unfavored_string)
-            print("\n=====================================\n")
-            f = open("unfavorable_accounts.txt", "a", encoding="utf-8")
-            r = open("unfavorable_accounts.txt", "r", encoding="utf-8")
-            current_file = r.read()
-            if tweet.user.description not in current_file:
-                f.write(unfavored_string)
+        if (self.determine_bot_chance(tweet) >= 60):
+            return
+        if (self.determine_unfavorability(tweet) >= 60):
             return
         if not tweet.favorited:
             try:
@@ -63,7 +81,7 @@ class StreamListener(tweepy.StreamListener):
                 print("Tweet retweeted!")
             except:
                 print("Tweet retweetion failed")
-        print("\n=====================================\n")
+        print(self.return_divider())
 
     def on_error(self, status):
         print(f"Error detected: {status}")
